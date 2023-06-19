43b34d07b89fa986b1d282ed28e80080c0d4203a
Marius Pikzirnis <grausas@gmail.com>
2023-06-19 13:58:02 +0300

    added new routes and redirect if no matching routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import { Map } from "./pages/Map";
+import { Login } from "./pages/login";
 import "./App.css";
 import Header from "./components/Header/Header";
 
 function App() {
+  const location = useLocation();
+  const hideNavbar = location.pathname === "/login";
+
   return (
     <>
-      <Header />
+      {!hideNavbar && <Header />}
+
       <Routes>
         <Route path="/" element={<Map />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </>
   );
