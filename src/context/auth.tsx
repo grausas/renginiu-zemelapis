ae9acacf3d342aae2b1f700840e71c3f24696523
Marius Pikzirnis <grausas@gmail.com>
2023-06-20 14:25:37 +0300

    authentication with login page

---
+++ src/context/auth.tsx
@@ -0,0 +1,123 @@
+import React, { useState, createContext, useMemo, useEffect } from "react";
+
+// Create the context
+export const AuthContext = createContext({});
+
+// Define the user object type
+interface User {
+  token: string;
+  name: string;
+  expires: number;
+}
+
+interface Users {
+  username: string;
+  password: string;
+}
+
+// Define the props for the AuthProvider component
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+// Define the server information object
+const serverInfo: any = {
+  server: "https://www.arcgis.com",
+  tokenServiceUrl: "https://www.arcgis.com/sharing/generateToken",
+};
+
+const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User>({
+    token: "",
+    name: "",
+    expires: 0,
+  });
+
+  const importIdentifyManager = useMemo(async () => {
+    const esriId = (await import("@arcgis/core/identity/IdentityManager.js"))
+      .default;
+    return esriId;
+  }, []);
+
+  const login = async (users: Users) => {
+    console.log("hello");
+    console.log("users", users);
+    const esriId = await importIdentifyManager;
+    esriId.tokenValidity = 300;
+
+    esriId.generateToken(serverInfo, users).then((response) => {
+      response.server = serverInfo.server;
+      response.userId = users.username;
+      esriId.registerToken(response);
+      console.log("response", response);
+      esriId
+        .getCredential(
+          "https://services1.arcgis.com/usA3lHW20rGU6glp/ArcGIS/rest/services/Renginiai_Vilniuje_P/FeatureServer/0"
+        )
+        .then((res) => {
+          if (res) {
+            console.log("res", res);
+            const token = res.token;
+            const name = res.userId;
+            const expires = res.expires;
+            setUser({ token, name, expires });
+            localStorage.setItem(
+              "item",
+              JSON.stringify({
+                token,
+                name,
+                expires,
+              })
+            );
+          }
+        });
+    });
+  };
+
+  const loadUserFromLocalStorage = async () => {
+    const esriId = await importIdentifyManager;
+    const token = JSON.parse(localStorage.getItem("item") || "{}");
+
+    if (token.token && token.expires > Date.now()) {
+      esriId.registerToken({
+        token: token.token,
+        server:
+          "https://services1.arcgis.com/usA3lHW20rGU6glp/ArcGIS/rest/services/Renginiai_Vilniuje_P/FeatureServer/0",
+        expires: token.expires,
+      });
+
+      esriId
+        .checkSignInStatus(
+          "https://services1.arcgis.com/usA3lHW20rGU6glp/ArcGIS/rest/services/Renginiai_Vilniuje_P/FeatureServer/0"
+        )
+        .then((res) => {
+          setUser(
+            { token: res.token, name: token.name, expires: token.expires } || {
+              token: "",
+              name: "",
+              expires: 0,
+            }
+          );
+        });
+    } else {
+      localStorage.removeItem("item");
+      setUser({
+        token: "",
+        name: "",
+        expires: 0,
+      });
+    }
+  };
+
+  useEffect(() => {
+    loadUserFromLocalStorage();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
---
+++ src/pages/login.tsx
@@ -0,0 +1,78 @@
+import { useState, useContext } from "react";
+import {
+  Flex,
+  Heading,
+  Input,
+  Button,
+  InputGroup,
+  Stack,
+  FormControl,
+  InputRightElement,
+  Image,
+  FormLabel,
+} from "@chakra-ui/react";
+import { AuthContext } from "../context/auth";
+
+export function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+  const auth: any = useContext(AuthContext);
+
+  const handleLogin = () => {
+    auth.login(user);
+  };
+
+  const handleShowClick = () => setShowPassword(!showPassword);
+
+  return (
+    <Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
+      <Flex p={8} flex={1} align={"center"} justify={"center"}>
+        <Stack spacing={4} w={"full"} maxW={"md"}>
+          <Heading fontSize={"2xl"}>Prisijungti prie savo paskyros</Heading>
+          <FormControl id="email">
+            <FormLabel>Prisijungimo vardas</FormLabel>
+            <Input
+              type="username"
+              onChange={(e) => setUser({ ...user, username: e.target.value })}
+            />
+          </FormControl>
+          <FormControl id="password">
+            <FormLabel>Slaptažodis</FormLabel>
+            <InputGroup>
+              <Input
+                type={showPassword ? "text" : "password"}
+                onChange={(e) => setUser({ ...user, password: e.target.value })}
+              />
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                  {showPassword ? "Slėpti" : "Rodyti"}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </FormControl>
+          <Stack spacing={6}>
+            <Button
+              colorScheme={"blue"}
+              variant={"solid"}
+              onClick={handleLogin}
+            >
+              prisijungti
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image
+          alt={"Login Image"}
+          objectFit={"cover"}
+          src={
+            "https://images.unsplash.com/photo-1578845832207-b74a206ac25d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80"
+          }
+        />
+      </Flex>
+    </Stack>
+  );
+}
