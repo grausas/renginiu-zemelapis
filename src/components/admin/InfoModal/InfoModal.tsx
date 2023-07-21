16cba7ce2267bc3444d30b09eedc72b3a14c5709
Marius Pikzirnis <grausas@gmail.com>
2023-07-21 09:59:46 +0300

    info modal how to create new feature

---
+++ src/components/admin/InfoModal/InfoModal.tsx
@@ -0,0 +1,49 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  Tooltip,
+  ModalBody,
+  ModalCloseButton,
+  List,
+  ListItem,
+  ListIcon,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { InfoIcon, CheckCircleIcon } from "@chakra-ui/icons";
+
+export default function InfoModal() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Tooltip label="Informacija kaip kurti renginius">
+        <InfoIcon
+          boxSize="4"
+          onClick={onOpen}
+          position="absolute"
+          top="2"
+          left="2"
+          _hover={{
+            cursor: "pointer",
+          }}
+        />
+      </Tooltip>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Paaiškinimas kaip kurti naujus renginius</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <List>
+              <ListItem>
+                <ListIcon as={CheckCircleIcon} color="green.500" />
+                Nubraižyti žemėlapyje geometriją
+              </ListItem>
+            </List>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
